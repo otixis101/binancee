48f1ab4264859ee4e63fe4ac88787eafe542dca4
otixis101 <preciouostis100@gmail.com>
2023-09-16 09:30:19 +0100

    deploy fix 1.2

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,10 +1,12 @@
 import type { NextAuthOptions } from 'next-auth'
 import GoogleProvider from "next-auth/providers/google";
+import {db} from "@/lib/db"
 import {PrismaAdapter} from '@next-auth/prisma-adapter'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
 
 export const options: NextAuthOptions = {
+  adapter: PrismaAdapter(db),
     providers: [
       GoogleProvider({
         clientId: process.env.GOOGLE_ID ?? "",
@@ -12,9 +14,9 @@ export const options: NextAuthOptions = {
       }),
       
     ],
-    secret: process.env.NEXTAUTH_SECRET,
-    session: {
-        strategy: "jwt",
-    },
-    debug: process.env.NODE_ENV === "development",
+    // secret: process.env.NEXTAUTH_SECRET,
+    // session: {
+    //     strategy: "jwt",
+    // },
+    // debug: process.env.NODE_ENV === "development",
   };

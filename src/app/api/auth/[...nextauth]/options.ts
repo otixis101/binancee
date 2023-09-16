955c99d306c92668714f0975d35743fbf76890b0
otixis101 <preciouostis100@gmail.com>
2023-09-16 09:48:47 +0100

    deploy again

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -14,9 +14,9 @@ export const options: NextAuthOptions = {
       }),
       
     ],
-    // secret: process.env.NEXTAUTH_SECRET,
-    // session: {
-    //     strategy: "jwt",
-    // },
-    // debug: process.env.NODE_ENV === "development",
+    secret: process.env.NEXTAUTH_SECRET,
+    session: {
+        strategy: "jwt",
+    },
+    debug: process.env.NODE_ENV === "development",
   };
